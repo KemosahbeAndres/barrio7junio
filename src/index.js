b128f82d60cc9a550c864abaff513479a856d88b
Andres Cubillos <andrestj1996@hotmail.com>
2021-11-13 14:03:34 -0300

    3er try

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const https = require("https")
 const app = require("express")()
-const port = process.env.port || 80
-let server = https.createServer()
+app.set('port', process.env.port || 80)
 app.get('/', (req, res)=>{
     res.send("Hola")
 })
 
-app.listen(port, ()=>{
+app.listen(app.get('port'), ()=>{
     console.log("Server running at port:", port)
 })
