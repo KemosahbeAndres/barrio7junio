8b506b7f30d6681270b2f2481b065c6bbfcebb80
Andres Cubillos <andrestj1996@hotmail.com>
2021-11-13 16:08:20 -0300

    5to commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const https = require("https")
 const app = require("express")()
-app.set('port', process.env.port || 80)
+app.set('port', process.env.port || 8080)
 app.get('/', (req, res)=>{
     res.send("Hola")
 })
 
-app.listen(app.get('port'), ()=>{
-    console.log("Server running at port:", app.get('port'))
-    
-})
+app.listen(app.get('port'))
