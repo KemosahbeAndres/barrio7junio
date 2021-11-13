6fbcd1c6cce589a772b5a848fc4f4ea7dad02023
Andres Cubillos <andrestj1996@hotmail.com>
2021-11-13 16:39:26 -0300

    some errors fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 app.set('port', process.env.port || 8080)
-app.use('/', express.static(__dirname + '/www'))
+app.use('/', express.static(__dirname + '/www/'))
 app.use('/api/', (req, res)=>{
     res.send("Welcome to api")
 })
