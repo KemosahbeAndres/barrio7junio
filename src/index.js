43f116649b536550bf652d9b1e04642d184a01c7
Andres Cubillos <andrestj1996@hotmail.com>
2021-11-13 16:33:51 -0300

    resolved problem for express library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const app = require("express")()
+const express = require("express")
+const app = express()
 app.set('port', process.env.port || 8080)
 app.use('/', express.static(__dirname + '/www'))
 app.use('/api/', (req, res)=>{
